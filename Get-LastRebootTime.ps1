763b003e626ae56c1090e2a1cac669fabde31a1d
Rod Stewart <rodstewart@gmail.com>
2016-05-19 22:17:06 +0000

    Base Functionality and Tests

--- Get-LastRebootTime.ps1
+++ Get-LastRebootTime.ps1
@@ -1,7 +1,62 @@
 #requires -version 5.0
 
 #region Main
+function Get-LastRebootTime
+{
+    <#
+        .Synopsis
+        Returns the time a server last rebooted
+        .DESCRIPTION
+        Returns a System.DateTime object representing the last time and date a server rebooted
+        .EXAMPLE
+        Get-LastRebootTime -ComputerName SERVER01
+        .INPUTS
+        System.String
+        .OUTPUTS
+        System.DateTime
+    #>
 
-
+    [CmdletBinding(DefaultParameterSetName='DefaultParameterSet')]
+    [OutputType([System.DateTime])]
+    Param
+    (
+        [Parameter(ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   Position=0,
+                   ParameterSetName='DefaultParameterSet')]
+        [ValidateNotNull()]
+        [Alias("ServerName")] 
+        [string[]]$ComputerName = 'localhost'
+    )
+    Process
+    {
+        foreach ($c in $ComputerName) { 
+            try {
+                $results = Get-CimInstance -Class Win32_OperatingSystem -ComputerName $c -ErrorAction Stop | Select-Object -ExpandProperty LastBootupTime
+                return $results
+            }
+            catch [System.Management.Automation.CommandNotFoundException]
+            {
+                Write-Warning -Message $_.exception
+                Throw 'Failure: CIM commands not found. Possibly running on or querying a WSMan / PowerShell 2.0 host' 
+            }
+            catch [Microsoft.Management.Infrastructure.CimException] { 
+                Write-Warning -Message $_.exception
+                Throw 'Failure: Unable to query host. Possibly querying a WSMan / PowerShell 2.0 host'
+            }
+        }
+    }
+}
 #endregion
 
+#TODO: 
+# This error occurs when you run against a machine that does not have PowerShell 5
+# Or maybe there's another cause - look this up
+#Get-CimInstance : The WS-Management service cannot process the request. A DMTF resource URI was used to access a
+#non-DMTF class. Try again using a non-DMTF resource URI.
+#At C:\Scripts\Get-LastRebootTime\Get-LastRebootTime.ps1:40 char:16
+#+ ...      return Get-CimInstance -Class Win32_OperatingSystem -ComputerNam ...
+#+                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#    + CategoryInfo          : NotSpecified: (root\cimv2:Win32_OperatingSystem:String) [Get-CimInstance], CimException
+#    + FullyQualifiedErrorId : HRESULT 0x80338139,Microsoft.Management.Infrastructure.CimCmdlets.GetCimInstanceCommand
+#    + PSComputerName        : trivirt13
--- Tests/Get-LastRebootTime.tests.ps1
+++ Tests/Get-LastRebootTime.tests.ps1
@@ -2,8 +2,79 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\..\$sut"
 
-Describe Get-LastRebootTime {
-    It "does something useful" {
-        $true | Should Be $true
+Describe Get-LastRebootTime {  
+    Context "Help and Parameter checks" {
+        Set-StrictMode -Version latest
+       
+        It 'Should have inbuilt help along with Description and examples' {
+            $helpinfo = Get-Help Get-LastRebootTime
+            $helpinfo.examples | Should not BeNullOrEmpty  # Should have examples
+            $helpinfo.Details | Should not BeNullOrEmpty   # Should have Details in the Help
+            $helpinfo.Description | Should not BeNullOrEmpty # Should have a Description for the Function
+        }
+    
+        It 'Should not accept Null ComputerName Mandatory params' {
+            #{Get-LastRebootTime} | Should Throw
+            {Get-LastRebootTime -computername $null } | Should throw
+        }
+
+        It 'Should return a System.DateTime object' {
+            $var = Get-LastRebootTime -ComputerName localhost
+            $var.gettype() | Should be "DateTime"
+        }
+       
+        It 'Should accept ServerName as an alias for ComputerName' {
+            {Get-LastRebootTime -servername localhost } | Should not throw 
+        }
+
+        It 'Should accept pipeline input' {
+            "localhost","localhost","localhost" | Get-LastRebootTime
+        }
+
+        It 'Should accept pipeline input by property name' {
+            $object = new-object -TypeName PSObject | select fake, unnecessary, computername
+            $object.computername = 'localhost'
+            $object | Get-LastRebootTime
+        }
+
+        It 'Should accept parameters by position' {
+            $var = Get-LastRebootTime localhost
+            $var.gettype() | Should be "DateTime"
+        }
+
+        It 'Should default ComputerName to localhost' {
+            Mock -CommandName Get-CIMInstance `
+            -Verifiable `
+            -ParameterFilter {
+                'computername' -eq 'localhost';
+                'class' -eq 'Win32_ComputerSystem';
+                'ErrorAction' -eq 'Stop'; 
+            }
+            Get-LastRebootTime #} | Should not throw 
+            Assert-VerifiableMocks
+        }
+    } # end Context
+    Context "CIM Commands Available" {
+        
+        Set-StrictMode -Version latest
+        
+        It "Should Fail if the CIM commands are not present" {
+            #Mock -CommandName Import-Module -ParameterFilter {$name -eq 'ActiveDirectory'} -MockWith {Throw (New-Object -TypeName System.IO.FileNotFoundException)} -Verifiable
+            #-ParameterFilter {$Class -eq 'Win32_OperatingSystem';$computername -eq 'localhost'}
+            $throwBlock = { Throw (New-Object -TypeName System.Management.Automation.CommandNotFoundException) }
+            $params = @{'commandname' = 'Get-CIMInstance'; 'mockWith' = $throwblock; 'Verifiable' = $true} 
+            Mock @params
+            {Get-LastRebootTime -ComputerName localhost } | Should throw
+            Assert-VerifiableMocks
+        }
+        
+        It "Should fail if you query a Powershell 2.0 Host" {
+            $throwBlock = { Throw (New-Object -TypeName Microsoft.Management.Infrastructure.CimException) }
+            $params = @{'commandname' = 'Get-CIMInstance'; 'mockWith' = $throwblock; 'Verifiable' = $true} 
+            Mock @params
+           {Get-LastRebootTime -ComputerName localhost } | Should throw
+            Assert-VerifiableMocks
+        }
+        
     }
 }
